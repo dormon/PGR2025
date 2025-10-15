93320cea34f38a549ad65f0f9a97b0670530dc3d
Tomáš Milet <imilet@fit.vutbr.cz>
2025-10-15 10:33:00 +0200

    createShader createProgram

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,42 @@
 
 using namespace ge::gl;
 
+GLuint createShader(GLenum type,std::string const&src){
+  auto vs = glCreateShader(type);
+  char const*srcs[]={
+    src.c_str(),
+  };
+  glShaderSource(vs,1,srcs,0);
+  glCompileShader(vs);
+  
+  GLint status;
+  glGetShaderiv(vs,GL_COMPILE_STATUS,&status);
+  if(status != GL_TRUE){
+    char buf[10000];
+    glGetShaderInfoLog(vs,10000,0,buf);
+    std::cerr << "ERROR: " << buf << std::endl;
+  }
+
+  return vs;
+}
+
+GLuint createProgram(std::vector<GLuint>const&shaders){
+  auto prg = glCreateProgram();
+  for(auto const&x:shaders)
+    glAttachShader(prg,x);
+  glLinkProgram(prg);
+
+  GLint status;
+  glGetProgramiv(prg,GL_LINK_STATUS,&status);
+  if(status != GL_TRUE){
+    char buf[10000];
+    glGetProgramInfoLog(prg,10000,0,buf);
+    std::cerr << "ERROR: " << buf << std::endl;
+  }
+
+  return prg;
+}
+
 int main(int argc,char*argv[]){
   auto window = SDL_CreateWindow("PGR2025",1024,768,SDL_WINDOW_OPENGL);
   auto context = SDL_GL_CreateContext(window);
@@ -12,33 +48,32 @@ int main(int argc,char*argv[]){
 
   auto vsSrc = R".(
   #version 460
+
+  out vec3 vColor;
+
   void main(){
-    if(gl_VertexID==0)gl_Position = vec4(0,0,0,1);
-    if(gl_VertexID==1)gl_Position = vec4(1,0,0,1);
-    if(gl_VertexID==2)gl_Position = vec4(0,1,0,1);
+    if(gl_VertexID==0){gl_Position = vec4(0,0,0,1);vColor = vec3(1,0,0);}
+    if(gl_VertexID==1){gl_Position = vec4(1,0,0,1);vColor = vec3(0,1,0);}
+    if(gl_VertexID==2){gl_Position = vec4(0,1,0,1);vColor = vec3(0,0,1);}
   }
   ).";
 
   auto fsSrc = R".(
   #version 460
+
+  in vec3 vColor;
   out vec4 fColor;
+
   void main(){
-    fColor = vec4(1,0,1,1);
+    fColor = vec4(vColor,1);
   }
   ).";
 
-  auto vs = glCreateShader(GL_VERTEX_SHADER);
-  glShaderSource(vs,1,&vsSrc,0);
-  glCompileShader(vs);
 
-  auto fs = glCreateShader(GL_FRAGMENT_SHADER);
-  glShaderSource(fs,1,&fsSrc,0);
-  glCompileShader(fs);
+  auto vs = createShader(GL_VERTEX_SHADER,vsSrc);
+  auto fs = createShader(GL_FRAGMENT_SHADER,fsSrc);
+  auto prg = createProgram({vs,fs});
 
-  auto prg = glCreateProgram();
-  glAttachShader(prg,vs);
-  glAttachShader(prg,fs);
-  glLinkProgram(prg);
 
 
   bool running = true;
@@ -49,7 +84,7 @@ int main(int argc,char*argv[]){
     }
   
     //rendering
-    glClearColor(0,0,0,1);
+    glClearColor(0.1,0.1,0.1,1);
     glClear(GL_COLOR_BUFFER_BIT);
 
     glPointSize(10);
